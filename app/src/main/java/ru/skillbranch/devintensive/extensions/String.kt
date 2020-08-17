5774f1be1c85b977b6e684ec367d2ef691f53aa5
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-08-17 20:30:40 +0300

    Реализован extension усекающий исходную строку до указанного числа символов (по умолчанию 16) и возвращающий усеченную строку с заполнителем "..." (если строка была усечена) если последний символ усеченной строки является пробелом - удалить его и добавить заполнитель

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,2 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
+fun String.truncate(value: Int = 16): String {
+    var string = this.trim()
+    val filling = if (string.length < value) "" else "..."
+    if (string.length >= value) {
+        string = string.substring(0 until value).trim()
+    }
+    return "${string}$filling"
+}
