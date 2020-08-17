ef0fcaa7d0d7a51b7ce7dcfed67de9e91f556635
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-08-17 14:31:03 +0300

    Реализован extension Date.format(pattern) возвращающий отформатированную дату по паттерну передаваемому в качестве аргумента (значение по умолчанию "HH:mm:ss dd.MM.yy" локаль "ru")

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,8 +1,19 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
 
-fun Date.humanizeDiff(pattern: String = ""): String {
-    //TODO
-    return ""
+/**
+ * Форматирует дату в виде строки согласно переданному шаблоку или использует шаблон по умолчанию.
+ *
+ * @param pattern шаблон форматирования даты. По умолчанию "HH:mm:ss dd.MM.yy" локаль RU.
+ * @return строку отформатированную согласно шаблона.
+ *
+ * Пример:
+ * Date().format() //14:00:00 27.06.19
+ * Date().format("HH:mm") //14:00
+ */
+fun Date.humanizeDiff(pattern: String = "HH:mm:ss dd.MM.yy"): String {
+    val dateFormat = SimpleDateFormat(pattern)
+    return dateFormat.format(this)
 }
