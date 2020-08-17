ba7cf68d3e6f61ef368b7b9d9ab78438f700d964
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-08-17 12:29:35 +0300

    Реализован класс BaseMessage

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -1,4 +1,14 @@
 package ru.skillbranch.devintensive.models
 
-class BaseMessage {
+import java.util.*
+
+abstract class BaseMessage(
+    val id: String,
+    val from: User?,
+    val chat: Chat,
+    val isIncoming: Boolean = false,
+    val date: Date = Date()
+) {
+
+    abstract fun formatMessage(): String
 }
