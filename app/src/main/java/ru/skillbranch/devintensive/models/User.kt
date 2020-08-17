e4afe0c1244b32e2376e710b3690529ed233adaa
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-08-17 22:11:55 +0300

    Реализован паттерн Builder для класса User.

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -38,4 +38,63 @@ data class User(
             return User(lastId.toString(), firstName, lastName)
         }
     }
+
+    /**
+     * Класс Builder для класса User.
+     *
+     */
+    class Builder {
+        private lateinit var id: String
+        private var firstName: String? = null
+        private var lastName: String? = null
+        private var avatar: String? = null
+        private var rating: Int = 0
+        private var respect: Int = 0
+        private var lastVisit: Date? = Date()
+        private var isOnline: Boolean = false
+
+        fun id(id: String): Builder  {
+            this.id = id
+            return this
+        }
+
+        fun firstName(firstName: String?): Builder {
+            this.firstName = firstName
+            return this
+        }
+
+        fun lastName(lastName: String?): Builder  {
+            this.lastName = lastName
+            return this
+        }
+
+        fun avatar(avatar: String?): Builder  {
+            this.avatar = avatar
+            return this
+        }
+
+        fun rating(rating: Int = 0): Builder  {
+            this.rating = rating
+            return this
+        }
+
+        fun respect(respect: Int = 0): Builder  {
+            this.respect = respect
+            return this
+        }
+
+        fun lastVisit(lastVisit: Date? = Date()): Builder  {
+            this.lastVisit = lastVisit
+            return this
+        }
+
+        fun isOnline(isOnline: Boolean = false): Builder  {
+            this.isOnline = isOnline
+            return this
+        }
+
+        fun build(): User {
+            return User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
+        }
+    }
 }
