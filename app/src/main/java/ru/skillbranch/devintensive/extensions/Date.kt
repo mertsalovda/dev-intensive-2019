b5e51f2e17cf2215715f7366563de7a9d93a6dc0
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-08-17 14:46:49 +0300

    Реализован extension Date.add(value, TimeUnits) добавляющий или вычитающий значение переданное первым аргументом в единицах измерения второго аргумента (enum TimeUnits [SECOND, MINUTE, HOUR, DAY]) и возвращающий модифицированный экземпляр Date

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -17,3 +17,25 @@ fun Date.humanizeDiff(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern)
     return dateFormat.format(this)
 }
+
+/**
+ * Добавляющий или вычитающий значение переданное первым аргументом
+ *
+ * @param value значение на которое будет изменено значение даты.
+ * @param unit единицах измерения enum TimeUnits [SECOND, MINUTE, HOUR, DAY].
+ * @return модифицированный экземпляр Date.
+ */
+fun Date.add(value: Int, unit: TimeUnits): Date {
+    var time = this.time
+    time += when (unit) {
+        TimeUnits.SECOND -> value * 1000
+        TimeUnits.MINUTE -> value * 60 * 1000
+        TimeUnits.HOUR -> value * 60 * 60 * 1000
+        TimeUnits.DAY -> value * 24 * 60 * 60 * 1000
+    }
+    return Date(time)
+}
+
+enum class TimeUnits {
+    SECOND, MINUTE, HOUR, DAY
+}
