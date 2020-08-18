4dceb71a3638852453b1d90fbe3bd47b22e951b4
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-08-18 18:01:41 +0300

    init

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
