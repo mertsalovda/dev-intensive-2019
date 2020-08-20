e8e35545cfc90d6145b9dd1a7765d6fad167cf51
mertsalovda <korranhorn009@gmail.com>
2020-08-20 21:28:46 +0300

    Испровлена строка с ответом в классе Bender

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -35,7 +35,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
             }
             else -> {
                 status = status.nextStatus()
-                "Это не правильный ответ\n${question.question}" to status.color
+                "Это неправильный ответ\n${question.question}" to status.color
             }
         }
     }
