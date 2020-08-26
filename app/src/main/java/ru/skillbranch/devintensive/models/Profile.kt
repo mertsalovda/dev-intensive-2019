d13bd55dad9287075a097d9c25c974babcdb98d3
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-08-26 14:17:20 +0300

    Реализовано Profile.nickName, как вычисляемое свойство из имени и фамилии пользователя, возвращающее значение псевдонима пользователя в виде транслитерированной строки с заменой пробела на "_"

--- app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.utils.Utils
+
 data class Profile(
         val firstName: String,
         val lastName: String,
@@ -9,7 +11,7 @@ data class Profile(
         val respect: Int = 0
 ) {
 
-    var nickName: String = "John Doe" // TODO implement me
+    var nickName: String = Utils.transliteration("$firstName $lastName", "_")
     var rank: String = "Junior Android Developer"
 
     /**
