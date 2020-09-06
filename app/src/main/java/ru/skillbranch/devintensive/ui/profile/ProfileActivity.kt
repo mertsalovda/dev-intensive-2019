ac7c0c4c91560e74cd6f27b4f92079ce56245c1c
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-09-06 19:52:19 +0300

    Изменён класс Profile firstName: String?, lastName: String?, about: String?, repository: String? теперь могут быть null.

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -214,10 +214,10 @@ class ProfileActivity : AppCompatActivity() {
 }
 
 data class Profile(
-        val firstName: String,
-        val lastName: String,
-        val about: String,
-        val repository: String,
+        val firstName: String?,
+        val lastName: String?,
+        val about: String?,
+        val repository: String?,
         val rating: Int = 0,
         val respect: Int = 0
 ) {
@@ -230,7 +230,7 @@ data class Profile(
      *
      * @return Map<K, V>
      */
-    fun toMap(): Map<String, Any> = mapOf(
+    fun toMap(): Map<String, Any?> = mapOf(
             "nickName" to nickName,
             "rank" to rank,
             "firstName" to firstName,
