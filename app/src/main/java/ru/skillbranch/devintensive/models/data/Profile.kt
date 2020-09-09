dad39ff75132212db4b1df609c3d3f01e9bd4090
mertsalovda <korranhorn009@gmail.com>
2020-09-09 21:32:48 +0300

    Создан класс Profile

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/data/Profile.kt
@@ -0,0 +1,32 @@
+package ru.skillbranch.devintensive.models.data
+
+import ru.skillbranch.devintensive.utils.Utils
+
+data class Profile(
+        val firstName: String?,
+        val lastName: String?,
+        val about: String?,
+        val repository: String?,
+        val rating: Int = 0,
+        val respect: Int = 0
+) {
+
+    var nickName: String = Utils.transliteration("$firstName $lastName".trim(), "_")
+    var rank: String = "Junior Android Developer"
+
+    /**
+     * Представляет объект [Profile] в виде [Map]
+     *
+     * @return Map<K, V>
+     */
+    fun toMap(): Map<String, Any?> = mapOf(
+            "nickName" to nickName,
+            "rank" to rank,
+            "firstName" to firstName,
+            "lastName" to lastName,
+            "about" to about,
+            "repository" to repository,
+            "rating" to rating,
+            "respect" to respect
+    )
+}
--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -12,6 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.data.Profile
 import ru.skillbranch.devintensive.utils.Utils
 import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
@@ -211,33 +212,4 @@ class ProfileActivity : AppCompatActivity() {
             viewModel.saveProfileDate(this)
         }
     }
-}
-
-data class Profile(
-        val firstName: String?,
-        val lastName: String?,
-        val about: String?,
-        val repository: String?,
-        val rating: Int = 0,
-        val respect: Int = 0
-) {
-
-    var nickName: String = Utils.transliteration("$firstName $lastName".trim(), "_")
-    var rank: String = "Junior Android Developer"
-
-    /**
-     * Представляет объект [Profile] в виде [Map]
-     *
-     * @return Map<K, V>
-     */
-    fun toMap(): Map<String, Any?> = mapOf(
-            "nickName" to nickName,
-            "rank" to rank,
-            "firstName" to firstName,
-            "lastName" to lastName,
-            "about" to about,
-            "repository" to repository,
-            "rating" to rating,
-            "respect" to respect
-    )
 }
